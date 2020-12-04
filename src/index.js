e990a9a55837c2140307e9bd09c1f5399a95db3c
guowangyang <guowangyang@tencent.com>
2020-12-04 21:33:50 +0800

    update: jsonp

--- src/index.js
+++ src/index.js
@@ -67,72 +67,84 @@ function bindStartInputChange(e) {
   computeDistance(startPointVal, endPointVal)
 }
 
+
+function createJsonp(url) {
+  const script = document.createElement('script')
+  document.body.appendChild(script)
+  script.src = url
+}
+
 // 解析地址为经纬度
 function parseAddress(val) {
   if (!val) return 
+  const output = 'jsonp'
 
-  const url = `${parseAddressURL}?key=${GEO_KEY}&address=${val}`
+  const url = `${parseAddressURL}?key=${GEO_KEY}&address=${val}&output=${output}`
   return new Promise((resolve, reject) => {
-    axios.get(url).then(res => {
+    jsonp(url).then(res => {
       if (res.status == 0 && res.result && res.result.location && res.result.location) {
         const { lat, lng } = res.result.location
-        const { city, district, province, street, street_number } = res.result.address_components
-
+  
         // 赋值解析经纬度
         getDom('parseLatLngResult').innerText = `${lat}, ${lng}`
-        // 赋值解析精确地址
-        getDom('parseAddressResult').innerText = `${province} ${city} ${district} ${street} ${street_number}`
+
+        if (res.result.address_components) {
+          const { city, district, province, street, street_number } = res.result.address_components
+          // 赋值解析精确地址
+          getDom('parseAddressResult').innerText = `${province} ${city} ${district} ${street} ${street_number}`
+        }
         resolve({ lat, lng })
       } else {
         reject()
-        alert(res.message)
       }
-    }).catch(() => {
-      reject()
     })
   })
+}
+
+function getIpLocationCb(res) {
   
 }
 
 // 根据IP获取地址
 function getIPLocation(ip) {
   return new Promise((resolve, reject) => {
-    let _url = `${tencentLocationURL}?key=${GEO_KEY}`
+    let _url = `${tencentLocationURL}?key=${GEO_KEY}&output=jsonp`
     if (ip) {
       _url += `&ip=${ip}`
     }
-
-    axios.get(_url).then(res => {
-      if (res.status == 0 && res.result) {
-        const { ip: _ip } = res.result
-        // 赋值IP
-        getDom('locationIpResult').innerText = _ip || ''
-
-        if (res.result.ad_info) {
-          const { ad_info: { nation, province, city, district, adcode } } = res.result
-          // 赋值地址
-          getDom('locationAddressResult').innerText = `${nation} ${province} ${city} ${district}` || ''
-          getDom('locationAdCodeResult').innerText = adcode || ''
-         
+    return new Promise((resolve, reject) => {
+      jsonp(_url).then(res => {
+        if (res.status == 0 && res.result) {
+          const { ip: _ip } = res.result
+          // 赋值IP
+          getDom('locationIpResult').innerText = _ip || ''
+      
+          if (res.result.ad_info) {
+            const { ad_info: { nation, province, city, district, adcode } } = res.result
+            // 赋值地址
+            getDom('locationAddressResult').innerText = `${nation} ${province} ${city} ${district}` || ''
+            getDom('locationAdCodeResult').innerText = adcode || ''
            
-          // 解析地址
-          parseAddress(city)
-          if (city) {
-            // 复制给解析输入框
-            addressInputDom.value = city
+             
+            if (city) {
+              // 复制给解析输入框
+              addressInputDom.value = city
+              // 解析地址
+              parseAddress(city)
+            }
           }
+          if (res.result.location) {
+            const { location: { lat, lng } } = res.result
+            // 赋值经纬度
+            getDom('locationLatLngResult').innerText = (lat && lng) ? `${lat},${lng}` : ``
+          }
+          
+        } else {
+          alert(res.message)
         }
-        if (res.result.location) {
-          const { location: { lat, lng } } = res.result
-          // 赋值经纬度
-          getDom('locationLatLngResult').innerText = (lat && lng) ? `${lat},${lng}` : ``
-        }
-        
-      } else {
-        alert(res.message)
-      }
-      resolve(res)
-    }).catch(() => resolve({}))
+      })
+    })
+    // createJsonp(_url)
   })
 }
 
@@ -157,9 +169,7 @@ function getLocation() {
       })
     } catch (e) {
       getIPLocation().then(resp => resolve(resp))
-
     }
-
   })
 }
 
@@ -175,8 +185,8 @@ function computeDistance(startVal, endVal) {
       const { lat, lng } = res2
       to = `${lat},${lng}`
 
-      const url = `${computeDistanceURL}?mode=${mode}&from=${from}&to=${to}&key=${GEO_KEY}`
-      axios.get(url).then(res => {
+      const url = `${computeDistanceURL}?mode=${mode}&from=${from}&to=${to}&key=${GEO_KEY}&output=jsonp`
+      jsonp(url).then(res => {
         if (res.status == 0 && res.result && res.result.rows && res.result.rows[0] && res.result.rows[0].elements && res.result.rows[0].elements[0]) {
           const { distance, duration } = res.result.rows[0].elements[0]
           if (distance > 3000) {
@@ -185,10 +195,7 @@ function computeDistance(startVal, endVal) {
             getDom('distanceResult').innerText = `${Math.round(distance / 1000)} 米`
           }
         }
-
       })
-
-
     })
   })
 
@@ -200,3 +207,40 @@ parseAddress(default_parse_address)
 computeDistance(startPointVal, endPointVal)
 
 
+function jsonp(url, data) {
+  return new Promise((resolve, reject) => {
+    // 初始化url
+    let dataString = url.indexOf('?') === -1 ? '?' : '&'
+    const randomNum = parseInt(Math.random()*10000)
+    let callbackName = `jsonpCB_${Date.now()}${randomNum}`
+    url += `${dataString}callback=${callbackName}`
+    if (data) {
+     // 有请求参数，依次添加到url
+      for (let k in data) {
+        url += `&${k}=${data[k]}`
+      }
+    }
+    
+    // 触发callback，触发后删除js标签和绑定在window上的callback
+    window[callbackName] = result => {
+      delete window[callbackName]
+      document.body.removeChild(jsNode)
+      if (result) {
+        resolve(result)
+      } else {
+        reject('没有返回数据')
+      }
+    }
+
+    let jsNode = document.createElement('script')
+    jsNode.src = url
+    // js加载异常的情况
+    jsNode.addEventListener('error', () => {
+      delete window[callbackName]
+      document.body.removeChild(jsNode)
+      reject('JavaScript资源加载失败')
+    }, false)
+    // 添加js节点到document上时，开始请求
+    document.body.appendChild(jsNode)
+  })
+}
